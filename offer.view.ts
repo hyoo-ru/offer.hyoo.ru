f5c6fe04317582e17848dc5ca76449fcb00e05ec
nin-jin <nin-jin@ya.ru>
2019-01-27 22:47:47 +0300

    initial

---
+++ offer.view.ts
@@ -0,0 +1,20 @@
+namespace $.$$ {
+
+	export class $hyoo_offer extends $.$hyoo_offer {
+
+		business() {
+			return this.$.$mol_state_arg.value( 'business' )
+		}
+
+		pages() {
+			return [
+				this.Main_page() ,
+				... ( this.business() === 'private' ? [ this.Private_page() ] : [] ) ,
+				... ( this.business() === 'corporate' ? [ this.Corporate_page() ] : [] ) ,
+				... ( this.business() === 'dev' ? [ this.Dev_page() ] : [] ) ,
+			]
+		}
+
+	}
+
+}
